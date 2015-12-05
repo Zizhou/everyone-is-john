cad972fcfa1a9708631ee412e5b5fe7a80908963
Daniel Ching <dan.t.ching@gmail.com>
2015-12-04 16:46:43 -0800

    dokku ready

--- views.py
+++ views.py
@@ -19,28 +19,49 @@ class ThreadView(View):
     def get(self, request, thread_id):
         posts = Thread.objects.get(id = thread_id).post_set.all()
         form = self.post_form
+        in_thread = _get_player(request.user, thread_id)
         context = {
+            'in_thread' : in_thread,
             'posts' : posts,
             'self_id' : thread_id,
             'post_form' : form, 
+            'active' : Thread.objects.get(id = thread_id).active_player,
         }
         return render(request, 'john/thread.html', context)
     def post(self, request, thread_id):
+        #branching to other actions
+        #wait, wait, shouldn't this be other functions in the class?!
+        if 'action_reply' in request.POST:
+            print 'action'
+            url = '/john/' + str(thread_id) + '/longreply/'
+            print url
+            return HttpResponse('action time!')
+            #return redirect(url)
+        if 'long_reply' in request.POST:
+            print 'long'
+            url = '/john/' + str(thread_id) + '/longreply/'
+            return HttpResponse('longform reply')
+            #return redirect(url)
+        in_thread = _get_player(request.user, thread_id)
         posts = Thread.objects.get(id = thread_id).post_set.all()
         form = self.post_form(request.POST)
         if form.is_valid():
             me = _get_player(request.user, thread_id)
             if me == False:
-                return HttpResponse('you done goofed' + str(request.user))
+                return HttpResponse('you done goofed, ' + str(request.user))
             form.make_post(thread_id, me)
 
         context = {
+            'in_thread' : in_thread,
             'posts' : posts,
             'self_id' : thread_id,
-            'post_form' : form, 
+            'post_form' : form,
+            'active' : Thread.objects.get(id = thread_id).active_player,
         }
         return render(request, 'john/thread.html', context)       
 
+
+#oh yeah, this name isn't confusing *at all*
 def post(request, post_id):
     return HttpResponse('post placeholder')
 
